913ae02d47cffa934a49567ad1cf12537b20bc88
Alon Grinshpoon <alon.grinshpoon@yahoo.com>
2015-07-20 14:57:48 +0300

    Type Choose Partial Redesign

--- src/il/co/togetthere/TypeChooserActivity.java
+++ src/il/co/togetthere/TypeChooserActivity.java
@@ -42,10 +42,12 @@ public class TypeChooserActivity extends Activity {
 		/**
 		 * View Initialization
 		 **/
-		View decorView = getWindow().getDecorView();
+
 		// Hide the status bar.
-		int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-		decorView.setSystemUiVisibility(uiOptions);
+		//View decorView = getWindow().getDecorView();
+		//int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+		//decorView.setSystemUiVisibility(uiOptions);
+		
 		// Remember that you should never show the action bar if the
 		// status bar is hidden, so hide that too if necessary.
 		android.app.ActionBar actionBar = getActionBar();
