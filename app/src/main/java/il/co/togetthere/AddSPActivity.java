6a5e19509083e306f25b399abdce57021a9d6064
Alon Grinshpoon <alon.grinshpoon@yahoo.com>
2015-07-21 12:23:37 +0300

    Edit/Add Screen Fonts

--- app/src/main/java/il/co/togetthere/AddSPActivity.java
+++ app/src/main/java/il/co/togetthere/AddSPActivity.java
@@ -7,17 +7,21 @@ import il.co.togetthere.db.ServiceProvider;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import com.facebook.widget.ProfilePictureView;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
@@ -50,8 +54,11 @@ public class AddSPActivity extends Activity {
 		//android.app.ActionBar actionBar = getActionBar();
 		//actionBar.hide();
 
+		// Define Font
+		Typeface font = Typeface.createFromAsset(getAssets(), "fonts/GOTHIC.TTF");
+
 		/**
-		 * Lower Bar
+		 * Upper Bar
 		 **/
 		ProfilePictureView profilePictureView = (ProfilePictureView) findViewById(R.id.button_show_user_details_addNewSP);
 		profilePictureView.setProfileId(LoginActivity.user.getID());
@@ -69,7 +76,48 @@ public class AddSPActivity extends Activity {
 		mSP = new ServiceProvider();
 		clientManager = new AmazonClientManager(this);
 
-		
+		/**
+		 * Titles
+		 */
+		TextView title = (TextView) findViewById(R.id.title_activity_add_sp);
+		title.setTypeface(font);
+
+		EditText addNewSPName = (EditText) findViewById(R.id.addNewSPName);
+		addNewSPName.setTypeface(font);
+
+		EditText addNewSPSPAddress = (EditText) findViewById(R.id.addNewSPSPAddress);
+		addNewSPSPAddress.setTypeface(font);
+
+		EditText addNewSPSPPhone = (EditText) findViewById(R.id.addNewSPSPPhone);
+		addNewSPSPPhone.setTypeface(font);
+
+		TextView title_addNewSPSpinner = (TextView) findViewById(R.id.title_addNewSPSpinner);
+		title_addNewSPSpinner.setTypeface(font);
+
+		EditText addNewSPDescription = (EditText) findViewById(R.id.addNewSPDescription);
+		addNewSPDescription.setTypeface(font);
+
+		EditText addNewSPDiscount = (EditText) findViewById(R.id.addNewSPDiscount);
+		addNewSPDiscount.setTypeface(font);
+
+		TextView addNewSPRankText = (TextView) findViewById(R.id.addNewSPRankText);
+		addNewSPRankText.setTypeface(font);
+
+		TextView textViewRank1 = (TextView) findViewById(R.id.textViewRank1);
+		textViewRank1.setTypeface(font);
+
+		TextView textViewRank2 = (TextView) findViewById(R.id.textViewRank2);
+		textViewRank2.setTypeface(font);
+
+		TextView textViewRank3 = (TextView) findViewById(R.id.textViewRank3);
+		textViewRank3.setTypeface(font);
+
+		TextView textViewRank4 = (TextView) findViewById(R.id.textViewRank4);
+		textViewRank4.setTypeface(font);
+
+		TextView textViewRank5 = (TextView) findViewById(R.id.textViewRank5);
+		textViewRank5.setTypeface(font);
+
 		/**
 		 * Type List Chooser
 		 **/
@@ -78,7 +126,7 @@ public class AddSPActivity extends Activity {
 		ArrayList<String> typesList = new ArrayList<>();
 		typesList.add(0, "Choose Type");
 		typesList.addAll(Arrays.asList(types));
-		mListAdapter = new ArrayAdapter<>(this, R.layout.type_list_item,
+		mListAdapter = new SpinnerAdapter(this, R.layout.type_list_item,
 				typesList);
 		mTypeList.setAdapter(mListAdapter);
 
@@ -108,122 +156,123 @@ public class AddSPActivity extends Activity {
 		/**
 		 * Submit Button- Validation
 		 **/
-		((Button) findViewById(R.id.addNewSPSubmit))
-				.setOnClickListener(new View.OnClickListener() {
+		Button submit = (Button) findViewById(R.id.addNewSPSubmit);
+		submit.setTypeface(font);
+		submit.setOnClickListener(new View.OnClickListener() {
 
+			@Override
+			public void onClick(View v) {
+
+				// Get Fields Content
+				String name = ((EditText) findViewById(R.id.addNewSPName))
+						.getText().toString();
+				String address = ((EditText) findViewById(R.id.addNewSPSPAddress))
+						.getText().toString();
+				String phone = ((EditText) findViewById(R.id.addNewSPSPPhone))
+						.getText().toString();
+				String description = ((EditText) findViewById(R.id.addNewSPDescription))
+						.getText().toString();
+				String discount = ((EditText) findViewById(R.id.addNewSPDiscount))
+						.getText().toString();
+				int discountNum = (discount.equals("") ? -1 : Integer
+						.parseInt(discount));
+
+				/**
+				 * Set SP
+				 **/
+
+				// Name
+				if (!name.equals("")) {
+					mSP.setName(name);
+				} else {
+					Toast.makeText(getApplicationContext(),
+							"Location name is missing",
+							Toast.LENGTH_SHORT).show();
+					return;
+				}
+
+				// Adress
+				if (!address.equals("")) {
+					mSP.setAddress(address);
+				} else {
+					Toast.makeText(getApplicationContext(),
+							"Location address is missing",
+							Toast.LENGTH_SHORT).show();
+					return;
+				}
+
+				// Phone
+				if (!phone.equals("")) {
+					mSP.setPhone(phone);
+				} else {
+					Toast.makeText(getApplicationContext(),
+							"Location phone is missing",
+							Toast.LENGTH_SHORT).show();
+					return;
+				}
+
+				// Description
+				if (!description.equals("")) {
+					// mSP.setDescription(phone);
+				}
+
+				// Discount
+				if (discountNum != -1) {
+					mSP.setDiscount(discountNum);
+				} else {
+					Toast.makeText(getApplicationContext(),
+							"Discount amount is missing",
+							Toast.LENGTH_SHORT).show();
+					return;
+				}
+
+				// Type
+				if (mSP.getType().equals("")) {
+					Toast.makeText(getApplicationContext(),
+							"You must select location type",
+							Toast.LENGTH_SHORT).show();
+					return;
+				}
+
+				// Re-Validate User (Are You Sure?)
+				DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
 					@Override
-					public void onClick(View v) {
+					public void onClick(DialogInterface dialog,
+										int which) {
+						switch (which) {
+							case DialogInterface.BUTTON_POSITIVE:
 
-						// Get Fields Content
-						String name = ((EditText) findViewById(R.id.addNewSPName))
-								.getText().toString();
-						String address = ((EditText) findViewById(R.id.addNewSPSPAddress))
-								.getText().toString();
-						String phone = ((EditText) findViewById(R.id.addNewSPSPPhone))
-								.getText().toString();
-						String description = ((EditText) findViewById(R.id.addNewSPDescription))
-								.getText().toString();
-						String discount = ((EditText) findViewById(R.id.addNewSPDiscount))
-								.getText().toString();
-						int discountNum = (discount.equals("") ? -1 : Integer
-								.parseInt(discount));
+								// OK message
+								Toast.makeText(getApplicationContext(),
+										"Thank You!", Toast.LENGTH_SHORT)
+										.show();
 
-						/**
-						 * Set SP
-						 **/
+								// send mSP to DB and finish;
+								DynamoDBManagerTask addProvider = new DynamoDBManagerTask();
+								addProvider.mSerivceProvider = mSP;
+								addProvider
+										.execute(DynamoDBManagerType.INSERT_SERVICE_PROVIDER
+												.toString());
 
-						// Name
-						if (!name.equals("")) {
-							mSP.setName(name);
-						} else {
-							Toast.makeText(getApplicationContext(),
-									"Location name is missing",
-									Toast.LENGTH_SHORT).show();
-							return;
+								// Close Activity
+								AddSPActivity.this.finish();
+								break;
+
+							case DialogInterface.BUTTON_NEGATIVE:
+								break;
 						}
-
-						// Adress
-						if (!address.equals("")) {
-							mSP.setAddress(address);
-						} else {
-							Toast.makeText(getApplicationContext(),
-									"Location address is missing",
-									Toast.LENGTH_SHORT).show();
-							return;
-						}
-
-						// Phone
-						if (!phone.equals("")) {
-							mSP.setPhone(phone);
-						} else {
-							Toast.makeText(getApplicationContext(),
-									"Location phone is missing",
-									Toast.LENGTH_SHORT).show();
-							return;
-						}
-
-						// Description
-						if (!description.equals("")) {
-							// mSP.setDescription(phone);
-						}
-
-						// Discount
-						if (discountNum != -1) {
-							mSP.setDiscount(discountNum);
-						} else {
-							Toast.makeText(getApplicationContext(),
-									"Discount amount is missing",
-									Toast.LENGTH_SHORT).show();
-							return;
-						}
-
-						// Type
-						if (mSP.getType().equals("")) {
-							Toast.makeText(getApplicationContext(),
-									"You must select location type",
-									Toast.LENGTH_SHORT).show();
-							return;
-						}
-
-						// Re-Validate User (Are You Sure?)
-						DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
-							@Override
-							public void onClick(DialogInterface dialog,
-									int which) {
-								switch (which) {
-								case DialogInterface.BUTTON_POSITIVE:
-
-									// OK message
-									Toast.makeText(getApplicationContext(),
-											"Thank You!", Toast.LENGTH_SHORT)
-											.show();
-
-									// send mSP to DB and finish;
-									DynamoDBManagerTask addProvider = new DynamoDBManagerTask();
-									addProvider.mSerivceProvider = mSP;
-									addProvider
-											.execute(DynamoDBManagerType.INSERT_SERVICE_PROVIDER
-													.toString());
-
-									// Close Activity
-									AddSPActivity.this.finish();
-									break;
-
-								case DialogInterface.BUTTON_NEGATIVE:
-									break;
-								}
-							}
-						};
-
-						AlertDialog.Builder builder = new AlertDialog.Builder(
-								AddSPActivity.this);
-						builder.setMessage(
-								"Are you sure you want to add " + name + "?")
-								.setPositiveButton("Yes", dialogClickListener)
-								.setNegativeButton("No", dialogClickListener)
-								.show();
-
 					}
+				};
+
+				AlertDialog.Builder builder = new AlertDialog.Builder(
+						AddSPActivity.this);
+				builder.setMessage(
+						"Are you sure you want to add " + name + "?")
+						.setPositiveButton("Yes", dialogClickListener)
+						.setNegativeButton("No", dialogClickListener)
+						.show();
+
+			}
 				});
 
 	}
@@ -257,6 +306,34 @@ public class AddSPActivity extends Activity {
 
 	private void updateServiceProviderRank(String rankType, boolean b) {
 		// TODO Auto-generated method stub
+	}
 
+	private static class SpinnerAdapter extends ArrayAdapter<String> {
+		// Initialise custom font, for example:
+		Typeface font = Typeface.createFromAsset(getContext().getAssets(), "fonts/GOTHIC.TTF");
+
+
+		// (In reality I used a manager which caches the Typeface objects)
+		// Typeface font = FontManager.getInstance().getFont(getContext(), BLAMBOT);
+
+		private SpinnerAdapter(Context context, int resource, List<String> items) {
+			super(context, resource, items);
+		}
+
+		// Affects default (closed) state of the spinner
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			TextView view = (TextView) super.getView(position, convertView, parent);
+			view.setTypeface(font);
+			return view;
+		}
+
+		// Affects opened state of the spinner
+		@Override
+		public View getDropDownView(int position, View convertView, ViewGroup parent) {
+			TextView view = (TextView) super.getDropDownView(position, convertView, parent);
+			view.setTypeface(font);
+			return view;
+		}
 	}
 }
--- app/src/main/java/il/co/togetthere/EditActivity.java
+++ app/src/main/java/il/co/togetthere/EditActivity.java
@@ -8,6 +8,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -15,6 +16,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.widget.ProfilePictureView;
@@ -31,17 +33,17 @@ public class EditActivity extends Activity {
 		/**
 		 * Full Screen Set UP
 		 **/
-		View decorView = getWindow().getDecorView();
 		// Hide the status bar.
-		int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-		decorView.setSystemUiVisibility(uiOptions);
+		// View decorView = getWindow().getDecorView();
+		// int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+		// decorView.setSystemUiVisibility(uiOptions);
 		// Remember that you should never show the action bar if the
 		// status bar is hidden, so hide that too if necessary.
 		//android.app.ActionBar actionBar = getActionBar();
 		//actionBar.hide();
 
 		/**
-		 * Lower Bar
+		 * Upper Bar
 		 **/
 		ProfilePictureView profilePictureView = (ProfilePictureView) findViewById(R.id.button_show_user_details_editSP);
 		profilePictureView.setProfileId(LoginActivity.user.getID());
@@ -63,129 +65,157 @@ public class EditActivity extends Activity {
 
 		clientManager = new AmazonClientManager(this);
 
+		// Define Font
+		Typeface font = Typeface.createFromAsset(getAssets(), "fonts/GOTHIC.TTF");
+
+		/**
+		 * Title
+		 */
+		TextView title = ((TextView) findViewById(R.id.title_activity_edit_sp));
+		title.setTypeface(font);
+
 		/**
 		 * Submit Button- Validation
 		 **/
+		TextView titleName = ((TextView) findViewById(R.id.title_editSPName));
+		titleName.setTypeface(font);
 		EditText name = ((EditText) findViewById(R.id.editSPName));
 		name.setText(mSP.getName());
+		name.setTypeface(font);
+
+		TextView titleAddress = ((TextView) findViewById(R.id.title_editSPSPAddress));
+		titleAddress.setTypeface(font);
 		EditText address = ((EditText) findViewById(R.id.editSPSPAddress));
 		address.setText(mSP.getAddress());
+		address.setTypeface(font);
+
+		TextView titlePhone = ((TextView) findViewById(R.id.title_editSPPhone));
+		titlePhone.setTypeface(font);
 		EditText phone = ((EditText) findViewById(R.id.editSPPhone));
+		phone.setTypeface(font);
 		phone.setText(mSP.getPhone());
+
+		TextView titleDescription = ((TextView) findViewById(R.id.title_editSPDescription));
+		titleDescription.setTypeface(font);
 		EditText description = ((EditText) findViewById(R.id.editSPDescription));
 		// TODO description.setText(mSP.getDescription());
+		description.setTypeface(font);
+
+		TextView titleDiscount = ((TextView) findViewById(R.id.title_editSPDiscount));
+		titleDescription.setTypeface(font);
 		EditText discount = ((EditText) findViewById(R.id.editSPDiscount));
-		discount.setText(""
-				+ mSP.getDiscount());
+		discount.setText("" + mSP.getDiscount());
+		discount.setTypeface(font);
 
-		((Button) findViewById(R.id.editSPSubmit))
-				.setOnClickListener(new View.OnClickListener() {
+		Button submit =  (Button) findViewById(R.id.editSPSubmit);
+		submit.setTypeface(font);
+		submit.setOnClickListener(new View.OnClickListener() {
 
+			@Override
+			public void onClick(View v) {
+
+				// Get Fields Content
+				String name = ((EditText) findViewById(R.id.editSPName))
+						.getText().toString();
+				String address = ((EditText) findViewById(R.id.editSPSPAddress))
+						.getText().toString();
+				String phone = ((EditText) findViewById(R.id.editSPPhone))
+						.getText().toString();
+				String description = ((EditText) findViewById(R.id.editSPDescription))
+						.getText().toString();
+				String discount = ((EditText) findViewById(R.id.editSPDiscount))
+						.getText().toString();
+				int discountNum = (discount.equals("") ? -1 : Integer
+						.parseInt(discount));
+
+				/**
+				 * Set SP
+				 **/
+
+				// Name
+				if (!name.equals("")) {
+					mSP.setName(name);
+				} else {
+					Toast.makeText(getApplicationContext(),
+							"Location name is missing",
+							Toast.LENGTH_SHORT).show();
+					return;
+				}
+
+				// Address
+				if (!address.equals("")) {
+					mSP.setAddress(address);
+				} else {
+					Toast.makeText(getApplicationContext(),
+							"Location address is missing",
+							Toast.LENGTH_SHORT).show();
+					return;
+				}
+
+				// Phone
+				if (!phone.equals("")) {
+					mSP.setPhone(phone);
+				} else {
+					Toast.makeText(getApplicationContext(),
+							"Location phone is missing",
+							Toast.LENGTH_SHORT).show();
+					return;
+				}
+
+				// Description
+				// if (!description.equals("")) {
+				// mSP.setDescription(phone);
+				// }
+
+				// Discount
+				if (discountNum != -1) {
+					mSP.setDiscount(discountNum);
+				} else {
+					Toast.makeText(getApplicationContext(),
+							"Discount amount is missing",
+							Toast.LENGTH_SHORT).show();
+					return;
+				}
+
+				// Re-Validate User (Are You Sure?)
+				DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
 					@Override
-					public void onClick(View v) {
+					public void onClick(DialogInterface dialog,
+										int which) {
+						switch (which) {
+							case DialogInterface.BUTTON_POSITIVE:
 
-						// Get Fields Content
-						String name = ((EditText) findViewById(R.id.editSPName))
-								.getText().toString();
-						String address = ((EditText) findViewById(R.id.editSPSPAddress))
-								.getText().toString();
-						String phone = ((EditText) findViewById(R.id.editSPPhone))
-								.getText().toString();
-						String description = ((EditText) findViewById(R.id.editSPDescription))
-								.getText().toString();
-						String discount = ((EditText) findViewById(R.id.editSPDiscount))
-								.getText().toString();
-						int discountNum = (discount.equals("") ? -1 : Integer
-								.parseInt(discount));
+								// OK message
+								Toast.makeText(getApplicationContext(),
+										"Thank You!", Toast.LENGTH_SHORT)
+										.show();
 
-						/**
-						 * Set SP
-						 **/
+								// update mSP in DB and finish;
+								DynamoDBManagerTask addProvider = new DynamoDBManagerTask();
+								addProvider.mSerivceProvider = mSP;
+								addProvider.execute(DynamoDBManagerType.INSERT_SERVICE_PROVIDER
+										.toString());
 
-						// Name
-						if (!name.equals("")) {
-							mSP.setName(name);
-						} else {
-							Toast.makeText(getApplicationContext(),
-									"Location name is missing",
-									Toast.LENGTH_SHORT).show();
-							return;
+								// Close Activity
+								EditActivity.this.finish();
+								break;
+
+							case DialogInterface.BUTTON_NEGATIVE:
+								break;
 						}
-
-						// Adress
-						if (!address.equals("")) {
-							mSP.setAddress(address);
-						} else {
-							Toast.makeText(getApplicationContext(),
-									"Location address is missing",
-									Toast.LENGTH_SHORT).show();
-							return;
-						}
-
-						// Phone
-						if (!phone.equals("")) {
-							mSP.setPhone(phone);
-						} else {
-							Toast.makeText(getApplicationContext(),
-									"Location phone is missing",
-									Toast.LENGTH_SHORT).show();
-							return;
-						}
-
-						// Description
-						// if (!description.equals("")) {
-						// mSP.setDescription(phone);
-						// }
-
-						// Discount
-						if (discountNum != -1) {
-							mSP.setDiscount(discountNum);
-						} else {
-							Toast.makeText(getApplicationContext(),
-									"Discount amount is missing",
-									Toast.LENGTH_SHORT).show();
-							return;
-						}
-
-						// Re-Validate User (Are You Sure?)
-						DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
-							@Override
-							public void onClick(DialogInterface dialog,
-									int which) {
-								switch (which) {
-								case DialogInterface.BUTTON_POSITIVE:
-
-									// OK message
-									Toast.makeText(getApplicationContext(),
-											"Thank You!", Toast.LENGTH_SHORT)
-											.show();
-
-									// update mSP in DB and finish;
-									DynamoDBManagerTask addProvider = new DynamoDBManagerTask();
-									addProvider.mSerivceProvider = mSP;
-									addProvider.execute(DynamoDBManagerType.INSERT_SERVICE_PROVIDER
-													.toString());
-
-									// Close Activity
-									EditActivity.this.finish();
-									break;
-
-								case DialogInterface.BUTTON_NEGATIVE:
-									break;
-								}
-							}
-						};
-
-						AlertDialog.Builder builder = new AlertDialog.Builder(
-								EditActivity.this);
-						builder.setMessage(
-								"Are you sure you want to add " + name + "?")
-								.setPositiveButton("Yes", dialogClickListener)
-								.setNegativeButton("No", dialogClickListener)
-								.show();
-
 					}
-				});
+				};
+
+				AlertDialog.Builder builder = new AlertDialog.Builder(
+						EditActivity.this);
+				builder.setMessage(
+						"Are you sure you want to add " + name + "?")
+						.setPositiveButton("Yes", dialogClickListener)
+						.setNegativeButton("No", dialogClickListener)
+						.show();
+
+			}
+		});
 
 	}
 
